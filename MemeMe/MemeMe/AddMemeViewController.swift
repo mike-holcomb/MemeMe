ed943242f23e12f6d27f0a00861e295407627671
mike-holcomb <mike-holcomb@users.noreply.github.com>
2015-07-07 23:34:41 -0500

    Add AddMemeViewController with pickImageFrom Album

---
+++ MemeMe/MemeMe/AddMemeViewController.swift
@@ -0,0 +1,43 @@
+//
+//  AddMemeViewController.swift
+//  MemeMe
+//
+//  Created by Michael Holcomb on 7/7/15.
+//  Copyright (c) 2015 Michael Holcomb. All rights reserved.
+//
+
+import UIKit
+
+class AddMemeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func pickAnImageFromAlbum(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+        
+        self.imageView.image = image
+        picker.dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
