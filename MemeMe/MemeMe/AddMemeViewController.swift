d27901a805e9ab1ccad9b98ee33f59d16807fc65
mike-holcomb <mike-holcomb@users.noreply.github.com>
2015-07-07 23:57:37 -0500

    Switch imagePicker to withMediInfo

--- MemeMe/MemeMe/AddMemeViewController.swift
+++ MemeMe/MemeMe/AddMemeViewController.swift
@@ -30,11 +30,12 @@ class AddMemeViewController: UIViewController, UIImagePickerControllerDelegate, 
         self.presentViewController(imagePicker, animated: true, completion: nil)
     }
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
-        
-        self.imageView.image = image
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.imageView.image = image
+            
+        }
         picker.dismissViewControllerAnimated(true, completion: nil)
-        
     }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
